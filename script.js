4542b99efafc82ab20229faaabf6e7b9bc3572ec
sw33ws <gillisl22@outlook.com>
2024-04-11 20:24:41 -0400

    done

---
+++ script.js
@@ -0,0 +1,17 @@
+// This will only play once
+
+// var typed = new Typed('#retyping', {
+//     strings: ['Cool', 'Great', 'Fun'],
+//     typeSpeed: 50,
+//     backSpeed: 50,
+// });
+
+// This will loop the effect
+
+var typed = new Typed('#retyping', {
+    strings: ['Cool', 'Great', 'Fun'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+});
